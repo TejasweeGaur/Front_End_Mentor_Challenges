0f37d44c72adeae9b3abf185fb0ed893fe64015b
Tejaswee Gaur <53988970+TejasweeGaur@users.noreply.github.com>
2023-02-25 23:39:43 +0530

    Changed the base for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Front-End_Mentor_Practice/"
+  base: "/Front_End_Mentor_Challenges/"
 });
